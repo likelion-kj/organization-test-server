706c5780fe2b53726ff1992d360796740803ef3e
lkj <ll530@hanmail.net>
2021-08-08 14:55:16 +0900

    조직원 테이블 생성

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 * JUnit
 
 ## 테이블 설계
-* 테이블명: organization
+* 테이블명: company
 
 |컬럼명|유형(type)|비고|
 |------|---|---|
@@ -17,7 +17,7 @@
 |컬럼명|유형(type)|비고|
 |------|---|---|
 |id |bigint |auto_increment|
-|organization_id |bigint | - |
+|company_id |bigint | - |
 |name|Varchar(200)|-|
 |code|Varchar(200)|-|
 
@@ -38,6 +38,22 @@
 |department1_id |bigint | - |
 |name|Varchar(200)|-|
 |code|Varchar(200)|-|
+
+* 테이블명: member
+
+|컬럼명|유형(type)|비고|
+|------|---|---|
+|id |bigint |auto_increment|
+|name|Varchar(200)|-|
+
+* 테이블명: member_organization
+
+|컬럼명|유형(type)|비고|
+|------|---|---|
+|member_id |bigint |-|
+|organization_type|Varchar(100)|-|
+|organization_id|bigint|-|
+|manager_yn|char(1)|-|
 
 ## 문제해결 전략
 * 데이터 저장을 위해 h2활용, 필요시 다른 데이터저장소를 활용하여 영구 저장 가능 (https://www.baeldung.com/spring-boot-h2-database)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,24 +1,26 @@
-DROP TABLE IF EXISTS organization;
+DROP TABLE IF EXISTS company;
 DROP TABLE IF EXISTS division;
 DROP TABLE IF EXISTS department1;
 DROP TABLE IF EXISTS department2;
+DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS member_organization;
 
-CREATE TABLE organization (
+CREATE TABLE company (
   id BIGINT auto_increment PRIMARY KEY,
   name VARCHAR(200) NOT NULL
 );
 
-INSERT INTO organization (name) VALUES  ('ABC 회사');
+INSERT INTO company (name) VALUES  ('ABC 회사');
 
 CREATE TABLE division (
   id BIGINT auto_increment PRIMARY KEY,
-  organization_id BIGINT NOT NULL,
+  company_id BIGINT NOT NULL,
   name VARCHAR(200) NOT NULL,
   code VARCHAR(200) NOT NULL
 );
 
-INSERT INTO division (organization_id, name, code) VALUES  (1, '경영지원본부', 'D100');
-INSERT INTO division (organization_id, name, code) VALUES  (1, 'SW 개발본부', 'D200');
+INSERT INTO division (company_id, name, code) VALUES  (1, '경영지원본부', 'D100');
+INSERT INTO division (company_id, name, code) VALUES  (1, 'SW 개발본부', 'D200');
 
 CREATE TABLE department1 (
   id BIGINT auto_increment PRIMARY KEY,
@@ -45,3 +47,63 @@ INSERT INTO department2 (department1_id, name, code) VALUES  (4, '비즈서비�
 INSERT INTO department2 (department1_id, name, code) VALUES  (4, '그룹웨어개발팀', 'D213');
 INSERT INTO department2 (department1_id, name, code) VALUES  (5, '플랫폼서비스팀', 'D221');
 INSERT INTO department2 (department1_id, name, code) VALUES  (5, '모바일개발팀', 'D222');
+
+CREATE TABLE member (
+  id BIGINT auto_increment PRIMARY KEY,
+  name VARCHAR(200) NOT NULL
+);
+
+INSERT INTO member (name) VALUES  ('사장1');
+INSERT INTO member (name) VALUES  ('경영1');
+INSERT INTO member (name) VALUES  ('SW1');
+INSERT INTO member (name) VALUES  ('인사1');
+INSERT INTO member (name) VALUES  ('인사2');
+INSERT INTO member (name) VALUES  ('인사3');
+INSERT INTO member (name) VALUES  ('총무1');
+INSERT INTO member (name) VALUES  ('총무2');
+INSERT INTO member (name) VALUES  ('법무1');
+INSERT INTO member (name) VALUES  ('법무2');
+INSERT INTO member (name) VALUES  ('플랫폼1');
+INSERT INTO member (name) VALUES  ('서비스1');
+INSERT INTO member (name) VALUES  ('개발1');
+INSERT INTO member (name) VALUES  ('개발2');
+INSERT INTO member (name) VALUES  ('개발3');
+INSERT INTO member (name) VALUES  ('개발4');
+INSERT INTO member (name) VALUES  ('개발5');
+INSERT INTO member (name) VALUES  ('개발6');
+INSERT INTO member (name) VALUES  ('개발7');
+INSERT INTO member (name) VALUES  ('개발8');
+INSERT INTO member (name) VALUES  ('개발9');
+INSERT INTO member (name) VALUES  ('개발10');
+
+CREATE TABLE member_organization (
+  member_id BIGINT NOT NULL,
+  organization_type VARCHAR(100) NOT NULL,
+  organization_id BIGINT NOT NULL,
+  manager_yn CHAR(1) NOT NULL
+);
+ALTER TABLE member_organization ADD PRIMARY KEY (member_id, organization_type, organization_id);
+
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (1, 'COMPANY', 1, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (2, 'DIVISION', 1, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (3, 'DIVISION', 2, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (4, 'DEPARTMENT1', 1, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (5, 'DEPARTMENT1', 1, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (6, 'DEPARTMENT1', 1, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (7, 'DEPARTMENT1', 2, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (8, 'DEPARTMENT1', 2, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (9, 'DEPARTMENT1', 3, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (10, 'DEPARTMENT1', 3, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (11, 'DEPARTMENT1', 4, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (11, 'DEPARTMENT2', 1, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (12, 'DEPARTMENT1', 5, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (13, 'DEPARTMENT2', 1, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (14, 'DEPARTMENT2', 1, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (15, 'DEPARTMENT2', 2, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (16, 'DEPARTMENT2', 2, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (17, 'DEPARTMENT2', 3, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (18, 'DEPARTMENT2', 3, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (19, 'DEPARTMENT2', 4, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (20, 'DEPARTMENT2', 4, 'N');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (21, 'DEPARTMENT2', 5, 'Y');
+INSERT INTO member_organization (member_id, organization_type, organization_id, manager_yn) VALUES  (22, 'DEPARTMENT2', 5, 'N');
